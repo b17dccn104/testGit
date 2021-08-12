c98417caac5dea1ac9872191c51b537199778a1d
Đạt Bùi Hoàng <99buidat@gmail.com>
2021-08-12 11:11:21 +0700

    commit2

--- app/src/main/java/com/example/testgit/MainActivity.kt
+++ app/src/main/java/com/example/testgit/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    private  var b = 1
     private var a = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
